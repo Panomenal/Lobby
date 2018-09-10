b759ec62cb066fcb2194965bf9b62991e1e4d01d
czd <czd@Blo0dR0gue.panomenal.local>
2018-09-10 19:52:05 +0200

    Main Class

---
+++ Lobby/src/de/Panomenal/Lobby/Lobby.java
@@ -0,0 +1,17 @@
+package de.Panomenal.Lobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lobby extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
